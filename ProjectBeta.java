94ddc6aee816d22a9f6dc2f048f97cb7ea1b6592
zer0leet <diwakar29buddy@gmail.com>
2016-06-03 12:22:00 +0545

    Ascending Order

---
+++ ProjectBeta.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zer0leet;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Deepika
+ */
+public class ProjectBeta {
+
+    public static void main(String[] args) {
+        int num, i, j, temp;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of integers to sort:");
+        num = input.nextInt();
+
+        int array[] = new int[num];
+
+        System.out.println("Enter " + num + " integers: ");
+
+        for (i = 0; i < num; i++) {
+            array[i] = input.nextInt();
+        }
+
+        for (i = 0; i < (num - 1); i++) {
+            for (j = 0; j < num - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted list of integers:");
+
+        for (i = 0; i < num; i++) {
+            System.out.println(array[i]);
+        }
